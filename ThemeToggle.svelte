2e25cbf680e7847ce5bc600d4390a5fa4e87b37d
Ludvig Lindblom <ludvig.lindblom@gmail.com>
2026-08-22 12:21:20 +0200

    refactor: replace raw SVG icons with Svelte components
    Remove inline SVG strings and {@html} rendering, add dedicated theme icon components, preserve currentColor styling, and eliminate the svelte/no-at-html-tags warning.

--- ThemeToggle.svelte
+++ ThemeToggle.svelte
@@ -1,11 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
-
-	const ThemeLight =
-		'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><circle cx="12" cy="12" r="5" fill="currentColor"/><path fill="currentColor" d="M21 13h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2ZM4 13H3a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2Zm13.66-5.66a1 1 0 0 1-.66-.29 1 1 0 0 1 0-1.41l.71-.71a1 1 0 1 1 1.41 1.41l-.71.71a1 1 0 0 1-.75.29ZM5.64 19.36a1 1 0 0 1-.71-.29 1 1 0 0 1 0-1.41l.71-.66a1 1 0 0 1 1.41 1.41l-.71.71a1 1 0 0 1-.7.24ZM12 5a1 1 0 0 1-1-1V3a1 1 0 0 1 2 0v1a1 1 0 0 1-1 1Zm0 17a1 1 0 0 1-1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1-1 1ZM6.34 7.34a1 1 0 0 1-.7-.29l-.71-.71a1 1 0 0 1 1.41-1.41l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1-.71.29Zm12.02 12.02a1 1 0 0 1-.7-.29l-.66-.71A1 1 0 0 1 18.36 17l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1-.71.24Z"/></svg>';
-
-	const ThemeDark =
-		'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M20.21 15.32A8.56 8.56 0 1 1 11.29 3.5a.5.5 0 0 1 .51.28.49.49 0 0 1-.09.57A6.46 6.46 0 0 0 9.8 9a6.57 6.57 0 0 0 9.71 5.72.52.52 0 0 1 .58.07.52.52 0 0 1 .12.53Z"/></svg>';
+  import ThemeLight from './ThemeLight.svelte';
+  import ThemeDark from './ThemeDark.svelte';
 
 	const THEMES = {
 		LIGHT: 'light',
@@ -62,9 +58,9 @@
 	hidden={hidden || null}
 >
 	{#if currentThemeSetting === THEMES.DARK}
-		{@html ThemeLight}
+		<ThemeLight />
 	{:else}
-		{@html ThemeDark}
+    <ThemeDark />
 	{/if}
 </button>
 
